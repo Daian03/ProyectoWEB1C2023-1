7f034667f1953687c830af095c4dc6c38e07e6ca
typing-words <c.andresr11@gmail.com>
2023-04-14 01:37:09 -0600

    Ultimo commit de la noche

--- src/main/java/com/proyecto/service/PedidoService.java
+++ src/main/java/com/proyecto/service/PedidoService.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.proyecto.service;
 
 import com.proyecto.domain.Pedido;
 import java.util.List;
 
-/**
- *
- * @author candr
- */
 public interface PedidoService {
    public List<Pedido> getPedidos();
    
